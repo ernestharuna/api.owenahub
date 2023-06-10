3dd4283b6d422e9c050489967738172e722a6ff1
Ernest Haruna <92730168+ernestharuna@users.noreply.github.com>
2023-06-10 01:25:28 +0100

    Auth Added space to create remember me token in db Defined routes in api/php

--- app/Http/Controllers/auth/LoginController.php
+++ app/Http/Controllers/auth/LoginController.php
@@ -11,28 +11,37 @@ class LoginController extends Controller
 {
     public function login(Request $request)
     {
+        $remember = $request->input('remember', false);
+
         $data = $request->validate([
             'email' => 'required|email',
             'password' => 'required'
         ]);
 
-        if (Auth::attempt($data)) {
-            /**
-             * @var User $user
-             */
-            $user = Auth::user();
-            $token = $user->createToken('main')->plainTextToken;
+        try {
+            if (Auth::attempt($data, $remember)) {
+                /**
+                 * @var User $user
+                 */
+                $user = Auth::user();
+                $token = $user->createToken('main')->plainTextToken;
+
+                return response([
+                    'user' => $user,
+                    'token' => $token
+                ]);
+            }
 
             return response([
-                'user' => $user,
-                'token' => $token
-            ]);
+                'message' => 'Email or Password is incorrect'
+            ], 422);
+        } catch (\Exception $e) {
+            return response([
+                'error' => 'An error occurred while processing the request'
+            ], 500);
         }
-
-        return response([
-            'message' => 'Email or Password is incorrect'
-        ], 422);
     }
+
 
     public function logout(Request $request)
     {
@@ -43,6 +52,6 @@ class LoginController extends Controller
         $user = $request->user();
         $user->currentAccessToken()->delete();
 
-        return response('', 204);
+        return response(null, 204);
     }
 }
