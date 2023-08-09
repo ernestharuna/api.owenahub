9a89fd5a6069bfd2373ffee18686affe49a9d6a8
Ernest Haruna <92730168+ernestharuna@users.noreply.github.com>
2023-08-09 16:27:39 +0100

    Created HasMnay relationship for Session

--- app/Models/Session.php
+++ app/Models/Session.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Session extends Model
 {
@@ -23,5 +24,10 @@ class Session extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function session_task(): HasMany
+    {
+        return $this->hasMany(SessionTask::class);
     }
 }
